a888be6c65a2c2c51b4f6bd27b8092e53100c51b
rafaelflk <146246231+rafaelflk@users.noreply.github.com>
2024-05-27 13:45:15 -0300

    Add files via upload

---
+++ Prova 01/script.js
@@ -0,0 +1,7 @@
+let nota01 = 8.0, peso01 = 3;
+let nota02 = 9.5, peso02 = 6;
+let nota03 = 7.0, peso03 = 5;
+
+let media = (nota01 * peso01 + nota02 * peso02 + nota03 * peso03) / (peso01 + peso02 + peso03);
+
+console.log("A média ponderada é " + media);
